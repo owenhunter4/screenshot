1a1371f775dfd9847cbabd9aa8fcdfc3ee8e3745
owenhunter4 <owenhunter@gmail.com>
2024-03-08 14:59:20 +0700

    9

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,23 @@
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const screenshot = require("./screenshot");
 
-exports.screenshot = onRequest(async (req, res) => {
+exports.screenshot = onRequest({ memory: "4GiB", timeoutSeconds: 540, }, async (req, res) => {
     if (req.method === "POST") {
-        if(req.body.url){
-           await screenshot.getImageUrl(req.body.url).then((url) => {
-                res.status(200).json({screenUrl:url});
+        console.log(Object.keys(req.body).length);
+        if (Object.keys(req.body).length > 0) {
+            const gData = await screenshot.getImageUrl(req.body).catch((err) => {
+                console.log(err);
+                res.status(400).send({ errmessage: err });
+                return;
             });
+            res.status(200).send(gData);
+            return;
+
+        } else {
+            res.status(400).send({ message: "Content cannot be empty" });
+            return;
         }
     }
+
     return res.send(req.method);
 });
--- functions/screenshot.js
+++ functions/screenshot.js
@@ -4,42 +4,70 @@ const { Storage } = require('@google-cloud/storage');
 const GOOGLE_CLOUD_PROJECT_ID = "screenshot-47f59";
 const BUCKET_NAME = "gs://screenshot-47f59.appspot.com";
 
-const getImageUrl = async (url) => {
-    console.log("url === > " + url);
-    const imgBuffer = await scrnShot(url, "screen");
+const getImageUrl = async (body) => {
+    const _body = {
+        url: body.url,
+        width: body.width,
+        height: body.height
+    };
+    console.log("url === > " + _body.url);
+    console.log("width === > " + _body.width);
+    console.log("height === > " + _body.height);
+    const imgBuffer = await scrnShot(_body, "screen");
     const dName = new Date().getTime();
     const imgUrl = await urlImage(imgBuffer, dName + "/screen--");
     console.log("imgUrl === > " + imgUrl);
-    
-    return imgUrl;
+
+    var dataJsonReturn = {};
+    if (imgUrl != ""){
+        dataJsonReturn = {
+            status : "success",
+            imgUrl : imgUrl,
+            date : new Date()
+        };
+    }else{
+        dataJsonReturn = {
+            status : "fail"
+
+        };
+    }
+
+        return dataJsonReturn;
 }
 
 //-----
-const scrnShot = async (url, content) => {
+const scrnShot = async (_body, content) => {
     const browser = await puppeteer.launch({
-        args: ['--no-sandbox']
+        args: ['--no-sandbox', "--disable-setuid-sandbox",'--disable-web-security']
     });
+    console.log(await browser.version());
     const page = await browser.newPage();
     // await page.setViewport({
     //     width: 1920,
     //     height: 1080,
     //     deviceScaleFactor: 1,
     // });
+    
+    await page.setCacheEnabled(false);
+    var responseWeb = null;
     switch (content) {
         case "screen":
             await page.setViewport({
-                width: 1920,
-                height: 1080,
+                width: _body.width ? parseInt(_body.width) : 1920,
+                height: _body.height ? parseInt(_body.height) : 1080,
                 deviceScaleFactor: 1,
             });
-            await page.goto(url, { waitUntil: 'networkidle2' });
+            responseWeb = await page.goto(_body.url, { waitUntil: 'networkidle2' });
             break;
         default:
-            await page.goto("https://www.google.com/search?q=" + stockname, { waitUntil: 'networkidle2' });
+            responseWeb = await page.goto("https://www.google.com/search?q=" + stockname, { waitUntil: 'networkidle2' });
             break;
 
 
     }
+    console.log(responseWeb);
+    // await page.setBypassCSP(true);
+    // await page.setContent("<meta charset='utf-8'>");
 
     const buffer = await page.screenshot();
 
@@ -79,17 +107,21 @@ const uploadBufferImage = async (file, buffer, filename) => {
                 }
             }
         };
-        file.save(buffer, cache_file_options, () => {
-            const downLoadPath =
-                "https://firebasestorage.googleapis.com/v0/b/"+GOOGLE_CLOUD_PROJECT_ID+".appspot.com/o/";
+        try {
+            file.save(buffer, cache_file_options, () => {
+                const downLoadPath =
+                    "https://firebasestorage.googleapis.com/v0/b/" + GOOGLE_CLOUD_PROJECT_ID + ".appspot.com/o/";
 
-            const imageUrl =
-                downLoadPath +
-                encodeURIComponent(filename) +
-                "?alt=media&token=" +
-                gTime;
-            resolve(imageUrl);
-        });
+                const imageUrl =
+                    downLoadPath +
+                    encodeURIComponent(filename) +
+                    "?alt=media&token=" +
+                    gTime;
+                resolve(imageUrl);
+            });
+        } catch (err) {
+            resolve("");
+        }
     })
 }
 
